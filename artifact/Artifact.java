dfbb6eb7ec72ea860e0edcc77782a3aef276df82
Molina <enetwarch@protonmail.com>
2025-01-15 14:21:29 +0800

    implemented suppliers

--- artifact/Artifact.java
+++ artifact/Artifact.java
@@ -2,7 +2,7 @@ package artifact;
 import data.Data;
 import data.Data.Stats;
 import data.Data.Type;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.Supplier;
@@ -14,128 +14,95 @@ public class Artifact {
     private Type artifactType;
     private Map<Stats, Double> mainStat;
     public Artifact() {
-        initializeType();
-        initializeMainStat();
+        this.artifactType = TYPE_SUPPLIER.get();
+        this.mainStat = MAIN_STAT_SUPPLIER.get();
     }
 
 
-    ////// ARTIFACT TYPE METHODS
+    ////// ARTIFACT TYPE SUPPLIER
     
 
-    private static final Supplier<Type> randomTypeSetter = () -> {
+    private final Supplier<Type> TYPE_SUPPLIER = () -> {
         // Generates a random artifact type.
         Type[] typeArray = Type.values();
         int randomType = random.nextInt(typeArray.length);
         return typeArray[randomType];
     };
 
-    private void initializeType() {
-        this.artifactType = randomTypeSetter.get();
-    }
+
+    ////// MAIN STAT SUPPLIERS
 
 
-    ////// MAIN STAT METHODS
-
-
-    // RNG for unequal chances.
-    private static float mainStatRNG() {
-        // Generates a number from 0 to 1 including decimal points.
-        // Perfect for determining chances.
-        return random.nextFloat();
-    }
-
-    // Main Setter method
-    private void initializeMainStatMap(Stats mainStatName) {
-        this.mainStat = new HashMap<>();
-        mainStat.put(mainStatName, Data.MAIN_STATS.get(mainStatName)[0]);
-    }
-
-    private void initializeMainStatFlower() {
+    private static final Supplier<Stats> FLOWER_MAIN_STAT_SUPPLIER = () -> {
         // Flower of Life
-        initializeMainStatMap(Data.Stats.HP); // 100% -> HP
-    }
+        return Stats.HP; // 100% -> HP
+    };
     
-    private void initializeMainStatPlume() {
+    private static final Supplier<Stats> PLUME_MAIN_STAT_SUPPLIER = () -> {
         // Plume of Death
-        initializeMainStatMap(Stats.ATK); // 100% -> ATK
-    }
+        return Stats.ATK; // 100% -> ATK
+    };
     
-    private void initializeMainStatSands() {
+    private static final Supplier<Stats> SANDS_MAIN_STAT_SUPPLIER = () -> {
         // Sands of Eon
-        float rng = mainStatRNG();
-        if (rng <= .2668) { // 26.68% -> HP%
-            initializeMainStatMap(Stats.HP_PERCENT);
-        } else if (rng <= .5334) { // 26.66% -> ATK%
-            initializeMainStatMap(Stats.ATK_PERCENT);
-        } else if (rng <= .8) { // 26.66% -> DEF%
-            initializeMainStatMap(Stats.DEF_PERCENT);
-        } else if (rng <= .9) { // 10.00% -> Energy Recharge%
-            initializeMainStatMap(Stats.ENERGY_RECHARGE);
-        } else { // 10.00% -> Elemental Mastery
-            initializeMainStatMap(Stats.ELEMENTAL_MASTERY);
-        }
-    }
+        float rng = random.nextFloat();
+        return rng <= .2668 ? Stats.HP_PERCENT : // 26.68% -> HP%
+        rng <= .5334 ? Stats.ATK_PERCENT : // 26.66% -> ATK%
+        rng <= .8 ? Stats.DEF_PERCENT : // 26.66% -> DEF%
+        rng <= .9 ? Stats.ENERGY_RECHARGE : // 10.00% -> Energy Recharge%
+        Stats.ELEMENTAL_MASTERY; // 10.00% -> Elemental Mastery
+    };
     
-    private void initializeMainStatGoblet() {
+    private static final Supplier<Stats> GOBLET_MAIN_STAT_SUPPLIER = () -> {
         // Goblet of Eonothem
-        float rng = mainStatRNG();
-        if (rng <= .1925) { // 19.25% -> HP%
-            initializeMainStatMap(Stats.HP_PERCENT);
-        } else if (rng <= .385) { // 19.25% -> ATK%
-            initializeMainStatMap(Stats.ATK_PERCENT);
-        } else if (rng <= .575) { // 19.00% -> DEF%
-            initializeMainStatMap(Stats.DEF_PERCENT);
-        } else if (rng <= .625) { // 5.00% -> Pyro DMG Bonus%
-            initializeMainStatMap(Stats.PYRO_DMG_BONUS);
-        } else if (rng <= .675) { // 5.00% -> Electro DMG Bonus%
-            initializeMainStatMap(Stats.ELECTRO_DMG_BONUS);
-        } else if (rng <= .725) { // 5.00% -> Cryo DMG Bonus%
-            initializeMainStatMap(Stats.CRYO_DMG_BONUS);
-        } else if (rng <= .775) { // 5.00% -> Hydro DMG Bonus%
-            initializeMainStatMap(Stats.HYDRO_DMG_BONUS);
-        } else if (rng <= .825) { // 5.00% -> Dendro DMG Bonus%
-            initializeMainStatMap(Stats.DENDRO_DMG_BONUS);
-        } else if (rng <= .875) { // 5.00% -> Anemo DMG Bonus%
-            initializeMainStatMap(Stats.ANEMO_DMG_BONUS);
-        } else if (rng <= .925) { // 5.00% -> Geo DMG Bonus%
-            initializeMainStatMap(Stats.GEO_DMG_BONUS);
-        } else if (rng <= .975) { // 5.00% -> Physical DMG Bonus%
-            initializeMainStatMap(Stats.PHYSICAL_DMG_BONUS);
-        } else { // 2.50% -> Elemental Mastery
-            initializeMainStatMap(Stats.ELEMENTAL_MASTERY);
-        }
-    }
+        float rng = random.nextFloat();
+        return rng <= .1925 ? Stats.HP_PERCENT : // 19.25% -> HP%
+        rng <= .385 ? Stats.ATK_PERCENT : // 19.25% -> ATK%
+        rng <= .575 ? Stats.DEF_PERCENT : // 19.00% -> DEF%
+        rng <= .625 ? Stats.PYRO_DMG_BONUS : // 5.00% -> Pyro DMG Bonus%
+        rng <= .675 ? Stats.ELECTRO_DMG_BONUS : // 5.00% -> Electro DMG Bonus%
+        rng <= .725 ? Stats.CRYO_DMG_BONUS : // 5.00% -> Cryo DMG Bonus%
+        rng <= .775 ? Stats.HYDRO_DMG_BONUS : // 5.00% -> Hydro DMG Bonus%
+        rng <= .825 ? Stats.DENDRO_DMG_BONUS : // 5.00% -> Dendro DMG Bonus%
+        rng <= .875 ? Stats.ANEMO_DMG_BONUS : // 5.00% -> Anemo DMG Bonus%
+        rng <= .925 ? Stats.GEO_DMG_BONUS : // 5.00% -> Geo DMG Bonus%
+        rng <= .975 ? Stats.PHYSICAL_DMG_BONUS : // 5.00% -> Physical DMG Bonus%
+        Stats.ELEMENTAL_MASTERY; // 2.50% -> Elemental Mastery
+    };
     
-    private void initializeMainStatCirclet() {
+    private static final Supplier<Stats> CIRCLET_MAIN_STAT_SUPPLIER = () -> {
         // Circlet of Logos
-        float rng = mainStatRNG();
-        if (rng <= .22) { // 22.00% -> HP%
-            initializeMainStatMap(Stats.HP_PERCENT);
-        } else if (rng <= .44) { // 22.00% -> ATK%
-            initializeMainStatMap(Stats.ATK_PERCENT);
-        } else if (rng <= .66) { // 22.00% -> DEF%
-            initializeMainStatMap(Stats.DEF_PERCENT);
-        } else if (rng <= .76) { // 10.00% -> CRIT Rate%
-            initializeMainStatMap(Stats.CRIT_RATE);
-        } else if (rng <= .86) { // 10.00% -> CRIT DMG%
-            initializeMainStatMap(Stats.CRIT_DMG);
-        } else if (rng <= .96) { // 10.00% -> Healing Bonus%
-            initializeMainStatMap(Stats.HEALING_BONUS);
-        } else { // 4.00% -> Elemental Mastery
-            initializeMainStatMap(Stats.ELEMENTAL_MASTERY);
-        }
+        float rng = random.nextFloat();
+        return rng <= .22 ? Stats.HP_PERCENT : // 22.00% -> HP%
+        rng <= .44 ? Stats.ATK_PERCENT : // 22.00% -> ATK%
+        rng <= .66 ? Stats.DEF_PERCENT : // 22.00% -> DEF%
+        rng <= .76 ? Stats.CRIT_RATE : // 10.00% -> CRIT Rate%
+        rng <= .86 ? Stats.CRIT_DMG : // 10.00% -> CRIT DMG%
+        rng <= .96 ? Stats.HEALING_BONUS : // 10.00% -> Healing Bonus%
+        Stats.ELEMENTAL_MASTERY; // 4.00% -> Elemental Mastery
+    };
+
+    private static final Map<Type, Supplier<Stats>> MAIN_STAT_MAP = new EnumMap<>(Type.class);
+    static {
+        MAIN_STAT_MAP.put(Type.FLOWER, FLOWER_MAIN_STAT_SUPPLIER);
+        MAIN_STAT_MAP.put(Type.PLUME, PLUME_MAIN_STAT_SUPPLIER);
+        MAIN_STAT_MAP.put(Type.SANDS, SANDS_MAIN_STAT_SUPPLIER);
+        MAIN_STAT_MAP.put(Type.GOBLET, GOBLET_MAIN_STAT_SUPPLIER);
+        MAIN_STAT_MAP.put(Type.CIRCLET, CIRCLET_MAIN_STAT_SUPPLIER);
     }
-    
-    private void initializeMainStat() {
-        switch (artifactType) {
-            case FLOWER -> initializeMainStatFlower();
-            case PLUME -> initializeMainStatPlume();
-            case SANDS -> initializeMainStatSands();
-            case GOBLET -> initializeMainStatGoblet();
-            case CIRCLET -> initializeMainStatCirclet();
-        }
-    }
- 
+
+    private final Supplier<Map<Stats, Double>> MAIN_STAT_SUPPLIER = () -> {
+        mainStat = new EnumMap<>(Stats.class);
+        Stats mainStatName = MAIN_STAT_MAP.get(artifactType).get();
+        Double mainStatValue = Data.MAIN_STATS.get(mainStatName)[0];
+        mainStat.put(mainStatName, mainStatValue);
+        return mainStat;
+    };
+
+
+
+
+
     public void printSampleOutput() {
         StringBuilder sampleOutput = new StringBuilder();
         sampleOutput.append(String.format("%s\n\n", artifactType.getType().toUpperCase()));
@@ -143,7 +110,7 @@ public class Artifact {
             String mainStatName = mainStatEntry.getKey().getStat();
             double mainStatValue = mainStatEntry.getValue();
             sampleOutput.append("MAIN STAT\n");
-            sampleOutput.append(String.format("%-15s %.2f\n\n", mainStatName, mainStatValue));
+            sampleOutput.append(String.format("%-20s %.2f\n\n", mainStatName, mainStatValue));
         }
         System.out.print(sampleOutput);
     }
