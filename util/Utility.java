60e4663c6a6cb5e295376ca7f65b57383faeb02e
Molina <enetwarch@protonmail.com>
2025-01-18 21:13:57 +0800

    remove unused util methods

--- util/Utility.java
+++ util/Utility.java
@@ -1,5 +1,4 @@
 package util;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utility {
@@ -11,36 +10,6 @@ public class Utility {
     ////// INPUT METHODS
 
 
-    // Input helper method
-    private static void inputMessage(String message) {
-        System.out.printf("%s: ", message);
-    }
-
-    // getUserInputInt Helper method
-    private static void inputErrorInt(int min, int max) {
-        System.out.printf("%s. Accepts integers %d to %d.\n", INPUT_ERROR, min, max);
-    }
-
-    public static int getUserInputInt(String message, int min, int max) {
-        int userInput; 
-        while (true) {
-            try {
-                inputMessage(message);
-                userInput = scanner.nextInt(); 
-            } catch (InputMismatchException e) {
-                inputErrorInt(min, max); 
-                continue;
-            } finally {
-                scanner.nextLine(); // Absorbs \n
-            }
-            if (userInput >= min && userInput <= max) {
-                return userInput; 
-            } else {
-                inputErrorInt(min, max);
-            }
-        }
-    }
-
     public static boolean getUserInputBoolean(String message) {
         String userInput;
         while (true) {
@@ -51,41 +20,6 @@ public class Utility {
                 case "n" -> { return false; }
                 default -> System.out.printf("%s. Accepts y or n.\n", INPUT_ERROR);
             }
-        }
-    }
-
-    public static void inputBuffer() {
-        // Gives users time to read printed output.
-        System.out.print("Press enter to proceed. ");
-        scanner.nextLine();
-        System.out.print("\n");
-    }
-
-
-    ////// OUTPUT METHODS 
-
-
-    public static void printNumberedArray(String[] array) {
-        StringBuilder numberedArray = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            numberedArray.append(String.format("%d %s\n", i + 1, array[i]));
-        }
-        System.out.print(numberedArray);
-    }
-
-    public static int arraySumInt(int[] array) {
-        int arraySum = 0;
-        for (int number : array) {
-            arraySum += number;
-        }
-        return arraySum;
-    }
-
-    public static String pluralizeNoun(int amount) {
-        if (amount != 1) {
-            return "s";
-        } else {
-            return "";
         }
     }
 
