f70b3a7bc5a0c1788d7fe4b055538de3e260a4f1
Molina <enetwarch@protonmail.com>
2025-01-23 14:28:02 +0800

    added terminate program method

--- Main.java
+++ Main.java
@@ -3,6 +3,15 @@ import util.Utility;
 
 public class Main {
     
+    private static void terminateProgram() {
+        StringBuilder terminateProgram = new StringBuilder();
+        terminateProgram.append("Genshin Artifact Generator\n");
+        terminateProgram.append("Code by: Enetwarch\n");
+        System.out.print(terminateProgram);
+        Utility.scanner.close();
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
         while (true) {
             Rolls sampleArtifact = new Rolls();
@@ -10,7 +19,7 @@ public class Main {
             boolean proceedOrNot = Utility.getUserInputBoolean("Make another artifact");
             System.out.print("\n");
             if (!proceedOrNot) {
-                break;
+                terminateProgram();
             }
         }
     }
